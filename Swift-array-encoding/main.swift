5e4596a6bfcd22de367e400ce3314327508728c6
Sylvain Gamel <sylvain@SGinTech.fr>
2015-02-19 22:19:07 +0100

    Initial Commit

---
+++ Swift-array-encoding/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift-array-encoding
+//
+//  Created by Sylvain on 19/02/2015.
+//  Copyright (c) 2015 S.G. inTech. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
